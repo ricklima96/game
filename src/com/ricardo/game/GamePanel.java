4dcdcf6addd93d7fdb7c7e7e625bd5ef21fe48eb
Ricardo Lima <ricardo@MacBook-Air-de-Ricardo.local>
2018-10-15 23:55:11 -0300

    second commit

--- src/com/ricardo/game/GamePanel.java
+++ src/com/ricardo/game/GamePanel.java
@@ -1,6 +1,8 @@
 package com.ricardo.game;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
@@ -36,10 +38,64 @@ public class GamePanel extends JPanel implements Runnable {
 	public void run() {
 		init();
 
+		final double GAME_HERTZ = 60.0;
+		final double TBU = 1000000000 / GAME_HERTZ; // time for update
+
+		final int MUBR = 5; // must update before render
+
+		double lastUpdateTime = System.nanoTime();
+		double lastRenderTime;
+
+		final double TARGET_FPS = 60;
+		final double TTBR = 1000000000 / TARGET_FPS; // total time before render
+
+		int frameCount = 0;
+		int lastSecondTime = (int) (lastUpdateTime / 1000000000);
+		int oldFrameCount = 0;
+
 		while (running) {
-			update();
+			double now = System.nanoTime();
+			int updateCount = 0;
+			while (((now - lastUpdateTime) > TBU) && (updateCount < MUBR)) {
+				update();
+				input();
+				lastUpdateTime += TBU;
+				updateCount++;
+			}
+
+			if (now - lastUpdateTime > TBU) {
+				lastUpdateTime = now - TBU;
+			}
+
+			input();
 			render();
 			draw();
+			lastRenderTime = now;
+			frameCount++;
+
+			int thisSecond = (int) (lastUpdateTime / 1000000000);
+			
+			if (thisSecond > lastSecondTime) {
+				if (frameCount != oldFrameCount) {
+					System.out.println("NEW SECOND" + thisSecond + " " + frameCount);
+					oldFrameCount = frameCount;
+				}
+				frameCount = 0;
+				lastSecondTime = thisSecond;
+			}
+
+			while (now - lastRenderTime < TTBR && now - lastUpdateTime < TBU) {
+				Thread.yield();
+
+				try {
+					Thread.sleep(1);
+				} catch (Exception e) {
+					System.out.println("ERROR YIELDING THREAD");
+				}
+				now = System.nanoTime();
+
+			}
+
 		}
 
 	}
@@ -52,20 +108,28 @@ public class GamePanel extends JPanel implements Runnable {
 
 	}
 
-	private int x;
-	
 	public void update() {
-		x++;
-		System.out.println(x);
+
+		
+	}
+
+	public void input() {
 
 	}
 
 	public void render() {
+		if (g != null) {
+			g.setColor(new Color(66, 134, 244));
+			g.fillRect(0, 0, width, height);
+
+		}
 
 	}
 
 	public void draw() {
-
+		Graphics g2 = (Graphics) this.getGraphics();
+		g2.drawImage(img, 0, 0, width, height, null);
+		g2.dispose();
 	}
 
 }
