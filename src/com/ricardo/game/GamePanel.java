cf80eb5159361f04edd8d5ea40da0f2b10d7c808
Ricardo Lima <ricardo@MacBook-Air-de-Ricardo.local>
2018-10-15 23:54:55 -0300

    first and second video

--- src/com/ricardo/game/GamePanel.java
+++ src/com/ricardo/game/GamePanel.java
@@ -1,32 +1,71 @@
 package com.ricardo.game;
 
 import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel implements Runnable{
-	
+public class GamePanel extends JPanel implements Runnable {
+
 	public static int width;
 	public static int height;
-	
+
 	private Thread thread;
-	
+	private boolean running = false;
+	private BufferedImage img;
+	private Graphics2D g;
+
 	public GamePanel(int width, int height) {
+		this.width = width;
+		this.height = height;
 		setPreferredSize(new Dimension(width, height));
 		setFocusable(true);
 		requestFocus();
 	}
-	
+
 	public void addNotify() {
 		super.addNotify();
-	}
-	
-	public void run() {
-		if(thread == null) {
+
+		if (thread == null) {
 			thread = new Thread(this, "GameThread");
 			thread.start();
 		}
-		
+	}
+
+	public void run() {
+		init();
+
+		while (running) {
+			update();
+			render();
+			draw();
+		}
+
+	}
+
+	public void init() {
+		running = true;
+
+		img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		g = (Graphics2D) img.getGraphics();
+
+	}
+
+	private int x;
+	
+	public void update() {
+		x++;
+		System.out.println(x);
+
+	}
+
+	public void render() {
+
+	}
+
+	public void draw() {
+
 	}
 
 }
